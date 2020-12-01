0b8c6e8afb10a20e96e974614bc4d095594c1935
Cassandra Zoe Mcwilliams <cassandrazoe@Cassandras-MBP.attlocal.net>
2020-12-01 04:10:07 -0500

    Initial commit

---
+++ StaticLayout/Star Wars.js
@@ -0,0 +1,28 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+  showSlides(n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 0} 
+  if (n < 0) {slideIndex = slides.length -1}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex].style.display = "block"; 
+  dots[slideIndex].className += " active";
+}
